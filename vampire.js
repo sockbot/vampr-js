2d0bf9fb49d0647235be134773de855e5fd48dd1
Andy <github@sockbot.com>
2019-08-09 12:57:51 -0700

    implemented Vampire class and methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let distance = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      distance++;
+    }
+    return distance;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal)
   }
 
   /** Stretch **/
@@ -36,7 +43,23 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let ancestors1 = this.allAncestors;
+    let ancestors2 = vampire.allAncestors;
+    let commonAncestors = ancestors1.filter(ancestor => {
+      return ancestors2.includes(ancestor);
+    });
+    return commonAncestors[0];
+  }
 
+  get allAncestors() {
+    let ancestors = [this]; // include self in list of ancestors
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      ancestors.push(currentVamp.creator);
+      currentVamp = currentVamp.creator;
+    }
+    ancestors.push(currentVamp); // push root ancestor (root doesn't have a creator)
+    return ancestors;
   }
 }
 
